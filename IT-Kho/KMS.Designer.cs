f0a7379f1dffc774a9e41b7e4fa7e3553b9b7c76
thangbv <thangbv@CATALAN-C84>
2020-12-15 14:57:16 +0700

    commit KMS

--- IT-Kho/KMS.Designer.cs
+++ IT-Kho/KMS.Designer.cs
@@ -28,12 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(386, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // KMS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Name = "KMS";
             this.Size = new System.Drawing.Size(1057, 488);
             this.ResumeLayout(false);
@@ -41,5 +52,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
