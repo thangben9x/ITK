b716adccc5b96538e1c172a80044922360419e7a
thangbv <thangbv@CATALAN-C84>
2020-12-16 14:16:58 +0700

    CM B1

---
+++ IT-Kho/B1.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace IT_Kho
+{
+    partial class B1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // B1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "B1";
+            this.Size = new System.Drawing.Size(787, 440);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ IT-Kho/B1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace IT_Kho
+{
+    public partial class B1 : DevExpress.XtraEditors.XtraUserControl
+    {
+        public B1()
+        {
+            InitializeComponent();
+        }
+    }
+}
